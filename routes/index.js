0cb9c2e38fa04b41b5aa73b10bba7fbbfd95326b
yebin <yebin5127@gmail.com>
2021-12-01 17:03:15 +0900

    [Update] routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express")();
 const User = require("./user");
 const { Mail } = require("./mail");
+const Send = require("./send");
 
 router.use("/", User);
 router.use("/", Mail);
+router.use("/", Send);
 
 module.exports = router;
